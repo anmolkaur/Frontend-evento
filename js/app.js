4e27ef6299f9da5ae725714d5fb185b29cc984f6
anmolkaur <anmol.kaur18@gmail.com>
2014-01-26 16:14:23 -0500

    updated index.html

--- js/app.js
+++ js/app.js
@@ -2,16 +2,31 @@ var eventoApp = angular.module('eventoApp', []);
 
 eventoApp.controller('MyController', function ($scope) {
     $scope.categories = [
-    {"title":"Music", "subcategories":["Music1", "Music2", "Music3", "Music4", "Music5"]}, 
-    {"title":"Sports", "subcategories":["Sports1", "Sports2", "Sports3"]}, 
-    {"title":"Food", "subcategories":["Food1", "Food2", "Food3", "Food4", "Food5", "Food6"]}
+	{"title":"Music", "subcategories": [{"name":"M1", "events":["M1Event1", "M1Event2", "M1Event3"] }] },
+    {"title":"Sports", "subcategories":[{"name":"Sports1", "events":[] }] }, //, "Sports2", "Sports3"]}, 
+    {"title":"Food", "subcategories":[{"name": "Food1", "events":[] }] } //, "Food2", "Food3", "Food4", "Food5", "Food6"]}
     ];
    
     $scope.gotCategory = ["fakeCategory"];
+    $scope.gotSubcat = ["fakeSubcat"];
+    $scope.random = 3;
+
     $scope.subcategories = false;
+    $scope.events = false;
+
 
     $scope.categoryPicked = function(data) {
       $scope.gotCategory[0] = data;
       $scope.subcategories = true;
   };
+
+  $scope.subcatPicked = function(data) {
+      $scope.gotSubcat[0] = data;
+      $scope.events = true;
+      $scope.random = Math.floor(Math.random()*2);
+    };
+
+     $scope.done = function() {
+    
+    };
 });
